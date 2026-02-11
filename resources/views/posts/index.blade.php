ae5cbddf33e195c3cdc78b99ccb5618e470e003d
Tawose oluwatimilehin <oluwatimilehintawose2005@gmail.com>
2026-02-11 17:16:57 +0100

    show all post

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,7 +8,20 @@
 </head>
 
 <body>
-    <h3>Welcome to the post page </h3>
+    <h1>Welcome to the posts page </h1>
+    <a href="{{ route('posts.create') }}">Create a new post</a>
+    @foreach ($posts as $post)
+    <div>
+        <h2>{{ $post->title }}</h2>
+        <p>{{ $post->body}}</p>
+        <a href="{{ route('posts.edit', $post->id) }}">Edit</a>
+        <form action="{{ route('posts.destroy', $post->id) }}" method="post">
+            @csrf
+            @method('DELETE')
+            <button type="submit" onclick="return confirm('Are yo sure you want to delete this post?')">Delete</button>
+        </form>
+    </div>
+    @endforeach
 </body>
 
 </html>
