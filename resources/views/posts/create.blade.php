20d431a11d3641d9ba9042bc6f1ca08880998b46
Tawose oluwatimilehin <oluwatimilehintawose2005@gmail.com>
2026-02-11 17:16:25 +0100

    create post

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,11 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Create a Post</title>
 </head>
+
 <body>
     <h3>Create a post now!</h3>
+    <form action="{{ route('posts.store') }}" method="post">
+        @csrf
+        <label for="title">Title:</label>
+        <input type="text" name="title" value="{{ old('title') }}">
+        <label for="body">Body</label>
+        <textarea name="body">{{ old('body') }}</textarea>
+        <button type="submit">Save</button>
+        @if ($errors->any())
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+        @endif
+    </form>
 </body>
+
 </html>
